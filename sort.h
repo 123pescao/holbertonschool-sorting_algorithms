66b5766087d6e58141e16b8c5d8568a352a35b4d
AngelHCS <8990holbertonstudents.com>
2024-04-08 13:06:35 -0400

    commiting to branch before throwing to main

--- sort.h
+++ sort.h
@@ -11,6 +11,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
 const int n;
@@ -23,6 +24,7 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
-
+void quicksort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
-
